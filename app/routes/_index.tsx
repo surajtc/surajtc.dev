9ec1d54f1a31f33d0f8449dfcb913fc456755e66
Suraj T C <stelugar@umd.edu>
2024-02-06 21:58:39 -0500

    Update message

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
 import { ModeToggle } from "~/components/mode-toggle";
-import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,10 +10,22 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-      <Button>Click me</Button>
-      <ModeToggle />
-    </div>
+    <main className="flex flex-col min-h-screen">
+      <section className="border-b p-2">
+        <nav className="max-w-3xl mx-auto flex justify-between items-center">
+          <p className="text-xl font-semibold">surajtc.dev</p>
+          <ModeToggle />
+        </nav>
+      </section>
+      <section className="flex flex-col items-center justify-center grow">
+        <div>
+          <h1 className="text-4xl">Stay tuned...</h1>
+          <p className="text-lg font-light pt-2">
+            Big things are happening behind the scenes!
+          </p>
+        </div>
+      </section>
+      <section className="min-h-64"></section>
+    </main>
   );
 }
