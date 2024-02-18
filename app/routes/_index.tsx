f1129aa932cd44fa7304852ecde86123057c41c8
Suraj T C <stelugar@umd.edu>
2024-02-18 14:17:18 -0500

    Test image

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import profile from "~/images/profile.jpeg";
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,8 +16,20 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
-      <div>This is INDEX</div>
-    </>
+    <section className="flex flex-col gap-2 items-center md:flex-row-reverse md:items-start">
+      <div className="h-96 w-96 aspect-square bg-black rounded-full overflow-hidden">
+        <img
+          src={profile}
+          alt="profile"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <p className="">
+        Hello, I'm Suraj an aspiring Machine Learning Engineer. I'm a
+        recreational programmer and in this website I document anything and
+        everything about technologies that excites me. Join me for exploring
+        different technolioges together.
+      </p>
+    </section>
   );
 }
