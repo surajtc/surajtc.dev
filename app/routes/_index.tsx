5664f620ec9e269648ac42d0a4967b3605282aa6
suraj <stelugar@umd.edu>
2024-10-14 14:14:13 -0400

    Update dev vars

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,33 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  json,
+  LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "[surajtc.dev] Home" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  const uri = context.cloudflare.env.GITHUB_REDIRECT_URI || "Not Found";
+
+  return json({ uri });
+};
+
 export default function Index() {
   const [state, setState] = useState(0);
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div className="m-8">
+    <div className="m-8 space-y-4">
       <h1>Cloudflare Test</h1>
+      <p>Node Env: {process.env.NODE_ENV}</p>
+      <p>GitHub URI: {data.uri}</p>
       <div>
         <button
           className="bg-white text-black p-1 rounded"
